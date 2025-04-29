f9f81eec731faae0e35fb9298ba2cf0d798fcc57
unknown <zaidkhan262523@gmil.com>
2025-04-29 18:12:50 +0530

    first commit

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ mongoose.connect(process.env.MONGO_URI, {
 });
 
 app.use('/api', uploadRoute);
+app.get('/zaid',(req,res)=>{
+    res.send({message:"hello it live"});
+
+})
 
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
