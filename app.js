f15bfa61853e637309c5945f8b8f13b93245af58
unknown <zaidkhan262523@gmil.com>
2025-04-29 18:07:54 +0530

    first commit

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const uploadRoute = require('./routes/upload');
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use(cors({
+    origin: ['http://localhost:3001', 'https://your-frontend.vercel.app']
+  }));
+  
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
